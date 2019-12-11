ef43b9b584b05b17ce6e31416ffc43e9523ba892
Nguyen Trong Tan <tan.nt@sutrixsolutions.com>
2019-12-11 10:54:29 +0700

    2. Tìm hiểu về Vue Router  + 1. Install

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+
+import './plugins'  // Tự động tìm tới file index.js
+
+Vue.use(VueRouter)
+
+const routes = [
+]
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
