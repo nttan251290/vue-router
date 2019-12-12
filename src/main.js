c7b13516583bb467d6e3ce18b293a45e389b6a0c
Nguyen Trong Tan <tan.nt@sutrixsolutions.com>
2019-12-12 10:37:40 +0700

    2. Tìm hiểu về Vue Router  + 2. Router View

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 
+import Home from './pages/Home'
+import AboutUs from './pages/AboutUs'
+
 import './plugins'  // Tự động tìm tới file index.js
 
 Vue.use(VueRouter)
 
 const routes = [
+  { path: '/', component: Home },
+  { path: '/about-us', component: AboutUs }
 ]
 
 const router = new VueRouter({
