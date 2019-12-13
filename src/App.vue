7f8d5cfc4ddfb0e431595aa234b756d025af0c43
Nguyen Trong Tan <tan.nt@sutrixsolutions.com>
2019-12-13 11:22:39 +0700

    2. Tìm hiểu về Vue Router  + 10. Transition.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <comp-menu />
-    <router-view></router-view>
+    <transition name="slide-fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -48,6 +50,38 @@ li {
 
 a {
   color: #42b983;
+}
+
+/* .v-enter-active, .v-leave-active {
+  transition: all .3s ease
+}
+.v-enter {
+  opacity: 0;
+  transform: scale(1.1)
+}
+.v-enter-to {
+  opacity: 1;
+  transform: scale(1)
+}
+.v-leave {
+  opacity: 1;
+  transform: scale(1)
+}
+.v-leave-to {
+  opacity: 0;
+  transform: scale(1.1)
+} */
+
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
 }
 
 </style>
